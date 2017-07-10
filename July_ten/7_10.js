9b2f3c7bf67cb7dd9daad28f2a2007e52c4a6e0f
liushuyu <365889074@qq.com>
2017-07-10 19:18:41 +0800

    carousel one day

---
+++ July_ten/7_10.js
@@ -0,0 +1,18 @@
+//加分号是引入的jQuery最后封装没有结束符而导致出错，加一个以防错误发生
+;(function($){
+    var Carousel=function(poster){
+        alert(poster);
+    };
+    Carousel.prototype={
+
+    };
+    Carousel.init=function(posters){
+        var _this_=this;
+        posters.each(function(){
+            new _this_($(this));
+            //此处的this就是前面单个new里面的东西
+        });
+    };
+    //相当于注册，然后在html里面能够正常使用该对象
+    window["Carousel"]=Carousel; 
+})(jQuery);
